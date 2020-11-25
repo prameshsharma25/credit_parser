6a8fae84e49853a743c718eed86e4e89a7fd3da8
Pramesh Sharma <prameshsharma25@gmail.com>
2020-11-24 23:07:33 -0800

    scraped news data

--- credit.py
+++ credit.py
@@ -2,12 +2,18 @@ from bs4 import BeautifulSoup
 import requests
 
 # Grab html data from url
-url = "http://chase.com"
+url = "https://news.ycombinator.com/"
 page = requests.get(url)
 
 # Create BeautifulSoup object using html file
 soup = BeautifulSoup(page.text, "lxml")
 
+# Grab all article titles
+titles = [title.next_element for title in soup.find_all("a", "storylink")]
+scores = [score.next_element for score in soup.find_all("span", "score")]
+links = [link['href'] for link in soup.find_all("a", "storylink")]
+
 # Write html data into file.txt
 with open("file.txt", 'w') as f:
-    f.writelines(soup.prettify())
+    for item in range(0, len(titles)-1):
+        f.write(titles[item] + " " + links[item] + " " + scores[item] + "\n")
