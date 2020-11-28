e89a297df95245f47121b4078698692b021d1bbe
Pramesh Sharma <prameshsharma25@gmail.com>
2020-11-28 14:40:31 -0800

    functional to OOP

--- database.py
+++ database.py
@@ -1,12 +1,24 @@
 from pymongo import MongoClient
-from scraper import titles, links, scores
+from scraper import Scraper
 
-# Database to store scraped data
+# Create Mongo Database
 client = MongoClient()
 db = client.scraped_database
 
+# Call Scraper Object
+web_scraper = Scraper()
+
+#Store scraped data
+titles = web_scraper.scrape_article_titles()
+links = web_scraper.scrape_article_links()
+scores = web_scraper.scrape_article_scores()
+
 # Create collection
 collection = db.articles
+
+# Delete collection data
+if collection.count_documents({}) > 0:
+    collection.delete_many({})
 
 # Save scraped data in db
 for item in range(0, len(titles)-1):
@@ -17,3 +29,6 @@ for item in range(0, len(titles)-1):
     }
 
     collection.insert_one(data)
+
+# Sort articles by scores in descending order
+sorted_articles = collection.find().sort("score", -1)
--- scraper.py
+++ scraper.py
@@ -1,35 +1,35 @@
 from bs4 import BeautifulSoup
 import requests
 
-# Grab html data from url
-url = "https://news.ycombinator.com/"
-page = requests.get(url)
+class Scraper:
+    # Set up connection between BeautifulSoup and Ycombinator
+    def __init__(self):
+        self.url = "https://news.ycombinator.com/"
+        self.page = requests.get(self.url)       
+        self.soup = BeautifulSoup(self.page.text, "lxml")
 
-# Create BeautifulSoup object using html file
-soup = BeautifulSoup(page.text, "lxml")
+    # Grab article titles
+    def scrape_article_titles(self):
+        titles = [title.next_element for title in self.soup.find_all("a", "storylink")]
+        return titles
 
-# Grab article titles
-def scrape_article_titles():
-    titles = [title.next_element for title in soup.find_all("a", "storylink")]
-    return titles
+    # Grab article links
+    def scrape_article_links(self):
+        links  = [link['href'] for link in self.soup.find_all("a", "storylink")]
+        return links
 
-# Grab article links
-def scrape_article_links():
-    links  = [link['href'] for link in soup.find_all("a", "storylink")]
-    return links
+    # Grab article scores
+    def scrape_article_scores(self):
+        scores = []
+        temp_num = ""
+        for score in self.soup.find_all("span", "score"):
+            temp_score = score.next_element
+            for char in temp_score:
+                if char.isdigit():
+                    temp_num += char
+                else:
+                    scores.append(int(temp_num))
+                    temp_num = ""
+                    break
 
-# Grab article scores
-def scrape_article_scores():
-    scores = []
-    temp_num = ""
-    for score in soup.find_all("span", "score"):
-        temp_score = score.next_element
-        for char in temp_score:
-            if char.isdigit():
-                temp_num += char
-            else:
-                scores.append(int(temp_num))
-                temp_num = ""
-                break
-
-    return scores
+        return scores
