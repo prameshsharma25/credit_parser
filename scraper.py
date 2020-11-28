11ef4018ab61663a256ef99cf8227a8f9066a60a
Pramesh Sharma <prameshsharma25@gmail.com>
2020-11-27 21:11:46 -0800

    converted to functional

--- scraper.py
+++ scraper.py
@@ -8,7 +8,28 @@ page = requests.get(url)
 # Create BeautifulSoup object using html file
 soup = BeautifulSoup(page.text, "lxml")
 
-# Grab all article titles
-titles = [title.next_element for title in soup.find_all("a", "storylink")]
-scores = [score.next_element for score in soup.find_all("span", "score")]
-links  = [link['href'] for link in soup.find_all("a", "storylink")]
+# Grab article titles
+def scrape_article_titles():
+    titles = [title.next_element for title in soup.find_all("a", "storylink")]
+    return titles
+
+# Grab article links
+def scrape_article_links():
+    links  = [link['href'] for link in soup.find_all("a", "storylink")]
+    return links
+
+# Grab article scores
+def scrape_article_scores():
+    scores = []
+    temp_num = ""
+    for score in soup.find_all("span", "score"):
+        temp_score = score.next_element
+        for char in temp_score:
+            if char.isdigit():
+                temp_num += char
+            else:
+                scores.append(int(temp_num))
+                temp_num = ""
+                break
+
+    return scores
