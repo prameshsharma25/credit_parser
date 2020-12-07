6d54998baa230001443b770d301fbf659854fe84
Pramesh Sharma <prameshsharma25@gmail.com>
2020-12-06 21:28:48 -0800

    updated docstrings

--- scraper.py
+++ scraper.py
@@ -7,29 +7,15 @@ import ssl
 import getpass
 
 class Scraper:
-    """
-    This is a class for the Ycombinator web scraper.
-
-    Attributes:
-        url (str): Link to Ycombinator.
-        page (Object): Page content of Ycombinator site.
-        soup (Object): Connection to manipulate page data.
-    """
+    """This is a class for the Ycombinator web scraper."""
     def __init__(self) -> None:
-        """
-        The constructor for Scraper class.
-        """
+        """The constructor for Scraper class."""
         self.url = "https://news.ycombinator.com/"
         self.page = requests.get(self.url)       
         self.soup = BeautifulSoup(self.page.text, "lxml")
 
     def scrape_article_data(self) -> List[Dict[str, str]]:
-        """
-        Scrapes article titles, links, and scores data from Hacker News.
-
-        Returns:
-            articles (List[Dict[str, str]]): An array containing objects of scraped data.
-        """
+        """Scrapes article titles, links, and scores data from Hacker News."""
         articles = []
         links  = [link['href'] for link in self.soup.find_all("a", "storylink")]
         titles = [title.next_element for title in self.soup.find_all("a", "storylink")]
@@ -53,18 +39,7 @@ class Scraper:
         return articles
 
     def store_articles_in_database(self, articles: List[Dict[str, str]]) -> List[Dict[str, str]]:
-        """
-        Stores scraped data in mongo database.
-
-        Data consists of article titles, links, and scores and is stored in JSON format.
-        The data is stored in a mongoDB collection and then deleted if the collection stores prexisting data.
-
-        Parameters:
-            articles (List[Dict[str, str]]): An array containing objects of scraped data.
-
-        Returns:
-            collection (List[Dict[str, str]]): An array containing objects of scraped data.
-        """
+        """Stores article titles, links, and scores in mongo database."""
         client = MongoClient()
         db = client.scraped_database
 
@@ -82,36 +57,17 @@ class Scraper:
         return collection
 
     def sort_articles_by_score(self, collection: List[Dict[str, str]]) -> List[Dict[str, str]]:
-        """
-        Sorts article data in descending order by score.
-
-        Parameters:
-            collection (List[Dict[str, str]]): An array containing objects of scraped data.
-
-        Returns:
-            (List[Dict[str, str]]): A sorted array containing objects of scraped data.
-        """
+        """Sorts article data in descending order by score."""
         return [list(article.values()) for article in collection.find().sort("score", -1)]
 
     def write_articles_to_file(self, sorted_articles: List[Dict[str, str]]) -> None:
-        """
-        Writes article data into a file, which is temporarily stored.
-
-        Parameters:
-            sorted_articles (List[Dict[str, str]]): A sorted array containing objects of scraped data.
-        """
+        """Writes article data into a file, which is temporarily stored."""
         with open("email.txt", 'w') as f:
             for article in sorted_articles[0:4]:
                 f.write(f"Title: {article[1]} \nLink: {article[2]} \nPoints: {article[3]} \n\n")
 
     def send_articles_in_email(self) -> None:
-        """
-        Sends email of article data to myself.
-
-        Email is formatted using the most popular articles that were stored in 'email.txt' file.
-        Using the SMTP module, email is sent throught port 587 and SMTP server. A password is
-        required to login and is inputted using getpass module to hide character inputs in terminal.
-        """
+        """Sends email of article data to using and SMTP server."""
         port = 587
         smtp_server = "smtp.gmail.com"
         sender_email = "prameshsharma256@gmail.com"
@@ -128,9 +84,7 @@ class Scraper:
             server.sendmail(sender_email, receiver_email, message.encode("utf-8"))
 
 def main() -> None:
-    """
-    Functions of Scraper class are called inside main.
-    """
+    """Functions of Scraper class are called inside main."""
     scraper = Scraper()
     data = scraper.scrape_article_data()
     current_collection = scraper.store_articles_in_database(data)
