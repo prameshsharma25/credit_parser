7eb003fa5d327a4b5e144dd8346c22b50fe6116b
Pramesh Sharma <prameshsharma25@gmail.com>
2020-12-26 10:53:30 -0800

    PEP 8 protocol

--- scraper.py
+++ scraper.py
@@ -8,19 +8,22 @@ import smtplib
 import ssl
 import getpass
 
+
 class Scraper:
     """This is a class for the Ycombinator web scraper."""
+
     def __init__(self) -> None:
         """The constructor for Scraper class."""
         self.url = "https://news.ycombinator.com/"
-        self.page = requests.get(self.url)       
+        self.page = requests.get(self.url)
         self.soup = BeautifulSoup(self.page.text, "lxml")
 
     def scrape_article_data(self) -> List[Dict[str, str]]:
         """Scrapes article titles, links, and scores data from Hacker News."""
         articles = []
-        links  = [link['href'] for link in self.soup.find_all("a", "storylink")]
-        titles = [title.next_element for title in self.soup.find_all("a", "storylink")]
+        links = [link['href'] for link in self.soup.find_all("a", "storylink")]
+        titles = [title.next_element for title in self.soup.find_all(
+            "a", "storylink")]
         temp_num = ""
         index = 0
         for score in self.soup.find_all("span", "score"):
@@ -37,7 +40,7 @@ class Scraper:
                     temp_num = ""
                     index += 1
                     break
-        
+
         return articles
 
     def store_articles_in_database(self, articles: List[Dict[str, str]]) -> List[Dict[str, str]]:
@@ -48,10 +51,10 @@ class Scraper:
 
         if collection.count_documents({}) > 0:
             collection.delete_many({})
-            
+
         for article in articles:
             collection.insert_one(article)
-        
+
         return collection
 
     def sort_articles_by_score(self, collection: List[Dict[str, str]]) -> List[Dict[str, str]]:
@@ -62,7 +65,8 @@ class Scraper:
         """Writes article data into a file, which is temporarily stored."""
         with open("email.txt", 'w') as f:
             for article in sorted_articles[0:4]:
-                f.write(f"Title: {article[1]} \nLink: {article[2]} \nPoints: {article[3]} \n\n")
+                f.write(
+                    f"Title: {article[1]} \nLink: {article[2]} \nPoints: {article[3]} \n\n")
 
     def send_articles_in_email(self) -> None:
         """Sends email of article data to using and SMTP server."""
@@ -70,9 +74,8 @@ class Scraper:
         smtp_server = "smtp.gmail.com"
         sender_email = "prameshsharma256@gmail.com"
         receiver_email = "prameshsharma25@gmail.com"
-        #password = getpass.getpass(prompt="Password: ")
-        password = "agQp3q\"H5JN`F>J*>}*~"
-        
+        password = getpass.getpass(prompt="Password: ")
+
         with open("email.txt", "r") as f:
             message = f.read()
 
@@ -80,13 +83,15 @@ class Scraper:
         with smtplib.SMTP(smtp_server, port) as server:
             server.starttls(context=context)
             server.login(sender_email, password)
-            server.sendmail(sender_email, receiver_email, message.encode("utf-8"))
+            server.sendmail(sender_email, receiver_email,
+                            message.encode("utf-8"))
 
     def run_cron_jobs(self) -> None:
         """Runs the web scraper every morning."""
         schedule = BlockingScheduler()
         schedule.add_job(main, 'cron', day_of_week='0-6', hour='8')
         schedule.start()
+
 
 def main() -> None:
     """Functions of Scraper class are called inside main."""
@@ -97,6 +102,7 @@ def main() -> None:
     scraper.write_articles_to_file(sorted_articles)
     scraper.send_articles_in_email()
     scraper.run_cron_jobs()
+
 
 if __name__ == "__main__":
     main()
